413cb380628910f2f2090d9860558cdf5cb4779e
madhavchandok1 <madhavchandok.iimt@gmail.com>
2024-09-22 00:05:29 +0530

    Add authentication and user management features
    - Introduced AuthenticationController for user signup, added user roles and password encryption in UserService, and adjusted UsersController to update and delete users based on authentication.
    - Replaced saveEntry with saveUser for consistency and data integrity.

--- src/main/java/com/learning/journalApplication/controller/UsersController.java
+++ src/main/java/com/learning/journalApplication/controller/UsersController.java
@@ -5,41 +5,31 @@ import com.learning.journalApplication.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user")
 public class UsersController {
     @Autowired
     private UserService _userService;
 
-    @GetMapping
-    public List<User> getallUsers(){
-        return _userService.getAllUsers();
-    }
-
-    @GetMapping("/details/{userName}")
-    public User getUserDetails(@PathVariable String userName){
-        return _userService.findUserByUserName(userName);
-    }
-
-    @PostMapping("/createUser")
-    public void createUser(@RequestBody User user){
-        _userService.saveEntry(user);
-    }
-
-    @PutMapping("/updateUser/{userName}")
-    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable String userName){
+    @PutMapping("/updateUserDetails")
+    public ResponseEntity<?> updateUserDetails(@RequestBody User user){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = authentication.getName();
         User userInDb = _userService.findUserByUserName(userName);
-        if(userInDb != null){
-            userInDb.setUserName(user.getUserName());
-            userInDb.setPassword(user.getPassword());
-            _userService.saveEntry(userInDb);
-        }
+        userInDb.setUserName(user.getUserName());
+        userInDb.setPassword(user.getPassword());
+        _userService.saveNewUser(userInDb);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-
+    @DeleteMapping("/deleteUser")
+    public ResponseEntity<?> deleteUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        _userService.deleteUser(authentication.getName());
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
