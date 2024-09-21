c78ef67e18496aa1804d0fe8300f23f414e1f492
madhavchandok1 <madhavchandok.iimt@gmail.com>
2024-09-21 13:55:20 +0530

    Enable transaction management and externalize environment variables
    - Added `@Transactional` annotations to service methods for transaction management.
    - Introduced a new configuration class `TransactionConfiguration` to set up a `MongoTransactionManager`. Modified application properties to import settings from `dev.env.properties` and use environment variables for MongoDB configuration.
    - Updated `.gitignore` to include new configuration files.

--- src/main/java/com/learning/journalApplication/service/JournalEntryService.java
+++ src/main/java/com/learning/journalApplication/service/JournalEntryService.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -26,6 +27,7 @@ public class JournalEntryService {
         return journalEntry;
     }
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName){
         try{
             User user = _userService.findUserByUserName(userName);
@@ -43,6 +45,7 @@ public class JournalEntryService {
         _journalEntryRepository.save(journalEntry);
     }
 
+    @Transactional
     public void deleteEntry(ObjectId id, String userName){
         User user = _userService.findUserByUserName(userName);
         user.getJournalEntries().removeIf(journalEntry -> journalEntry.getId().equals(id));
